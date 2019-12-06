45700e04580aaa2e6e30e7922b6cb6ae98bdebb1
Fumiaki Kinoshita <fumiexcel@gmail.com>
2019-12-07 00:51:26 +0900

    Inline everything
    This improves ALL benchmark results by 2~5x by allowing primitive operations to be inlined. This should also make it realistic because the API are almost always called directly in reality.

--- library/ByteString/BuildersBenchmark/Subjects.hs
+++ library/ByteString/BuildersBenchmark/Subjects.hs
@@ -10,6 +10,8 @@ import qualified Data.ByteString.Lazy as D
 import qualified Data.ByteString.Builder as F
 import qualified Data.ByteString.FastBuilder as H
 import qualified Blaze.ByteString.Builder as I
+import qualified Mason.Builder as M
+import qualified Mason.Builder.Internal as M
 
 
 data Subject =
@@ -19,31 +21,39 @@ data Subject =
 byteStringStrictBuilder :: Subject
 byteStringStrictBuilder =
   Subject mempty mappend mconcat foldMap A.bytes A.builderBytes
+{-# INLINE byteStringStrictBuilder #-}
 
 byteStringTreeBuilder :: Subject
 byteStringTreeBuilder =
   Subject mempty mappend mconcat foldMap G.byteString G.toByteString
+{-# INLINE byteStringTreeBuilder #-}
 
 bufferBuilder :: Subject
 bufferBuilder =
   Subject (pure ()) (*>) sequenceA_ traverse_ B.appendBS B.runBufferBuilder
+{-# INLINE bufferBuilder #-}
 
 binary :: Subject
 binary =
   Subject mempty mappend mconcat foldMap C.putByteString (D.toStrict . C.runPut)
+{-# INLINE binary #-}
 
 cereal :: Subject
 cereal =
   Subject mempty mappend mconcat foldMap E.putByteString E.runPut
+{-# INLINE cereal #-}
 
 byteString :: Subject
 byteString =
   Subject mempty mappend mconcat foldMap F.byteString (D.toStrict . F.toLazyByteString)
+{-# INLINE byteString #-}
 
 fastBuilder :: Subject
 fastBuilder =
   Subject mempty mappend mconcat foldMap H.byteString H.toStrictByteString
+{-# INLINE fastBuilder #-}
 
 blazeBuilder :: Subject
 blazeBuilder =
   Subject mempty mappend mconcat foldMap I.fromByteString (D.toStrict . I.toLazyByteString)
+{-# INLINE blazeBuilder #-}
